fcd85a0a2cec858a82c80b4ad171c6df279da176
Kovak <kovac1066@gmail.com>
2016-06-07 23:42:17 -0600

    re order imports

--- modules/core/kivent_core/managers/__init__.py
+++ modules/core/kivent_core/managers/__init__.py
@@ -1,5 +1,5 @@
+from kivent_core.managers import game_manager
 from kivent_core.managers import resource_managers
 from kivent_core.managers import entity_manager
 from kivent_core.managers import system_manager
 from kivent_core.managers import sound_manager
-from kivent_core.managers import game_manager
